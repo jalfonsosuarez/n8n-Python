924c71d798ee75e48bf01be973a62c3765d84e51
= <=>
2026-01-22 11:56:01 +0100

    Organizando carpetas

--- n8n_python_seccion_5/.history/main_20260121124016.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-
-app = FastAPI(title="Image to WebP converter")
-
-@app.get("/test")
-def healt_check():
-    return {"status": "ok", "message": "Convertidor app funcionando"}
